b140bedf901bb1800aea779c58be78bffa865543
Pranay-Vaka <vakapranaya05@gmail.com>
2024-02-02 17:04:42 +0000

    something

--- kernel.c
+++ kernel.c
@@ -90,19 +90,6 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 void terminal_putchar(char c)
 {
 
-    // if (c == '\n')
-    // {
-    //     terminal_column = 0;
-    //     // if (++terminal_row >= VGA_HEIGHT)
-    //     // {
-    //     //     terminal_row = 0;
-    //     // }
-    //
-    //     // else {
-    //         terminal_row++;
-    //     // }
-    // }
-
     if (c == '\n') {
 
         terminal_column = 0;
@@ -147,7 +134,6 @@ void terminal_writestring(const char* data)
 void kernel_main(void)
 {
     terminal_initialise();
-
-    terminal_writestring("Hello, kernel World!\n");
-    terminal_writestring("Something in the way");
+    terminal_writestring("Hello, kernel World!\nSomething in the way");
 }
+
